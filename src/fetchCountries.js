89f5f8ee97a872a8fd41996ff692857e3cdffffe
Valov Ivan <pechenkooo3@gmail.com>
2022-12-08 21:17:16 +0200

    2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 import Notiflix from 'notiflix';
-function fetchCountries(name = 'Ukraine') {
+function fetchCountries(name) {
   return fetch(
-    `${BASE_URL}${name}?fields=name, capital,population,flags,languages`
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
   ).then(resp => {
     if (!resp.ok) {
       throw new Error(
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { Notify } from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries.js';
 var debounce = require('lodash.debounce');
@@ -9,46 +9,74 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('#country-list');
 const countryInfo = document.querySelector('#country-info');
+const countrySearch = document.querySelector('#search-box');
+document.getElementById('search-box').focus();
 
-inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+const refs = {
+  searchForm: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
   const countryName = e.target.value.trim();
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 
-  fetchCountries(countryName).then(data => {
-    if (!countryName) {
-      countryList.innerHTML = '';
-    } else if (data.length >= 2 && data.length <= 10) {
-      countryList.innerHTML = '';
-      createMurkupList(data);
-    } else if (data.length == 1) {
-      countryList.innerHTML = '';
-      createMurkupCard(data);
-    } else data.length > 10;
-    Notify.info('Too many matches found. Please enter a more specific name.');
-  });
+  if (!countryName) {
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(country => {
+      if (country.length > 10) {
+        return notifyInfoMessage();
+      }
+      if (country.length > 1 && country.length < 10) {
+        refs.countryList.innerHTML = createMurkupList(data);
+      } else {
+        refs.countryInfo.innerHTML = createMurkupCard(data);
+      }
+    })
+    .catch(error => notifyFailureMessage());
 }
 
 function createMurkupList(arr) {
-  const list = arr
-    .map(({ flags, name }) => {
-      return `<li class = 'country-item'>
-    <img srs ="${flags.svg}" alt ="${name.official}" width = "20x" height ="20px">
-    <p class = "country-name"> ${name.official}</p>
-    </li>`;
+  return arr
+    .map(country => {
+      return `
+        <li class="country__item">
+        <img class="country__img" src="${country.flags.svg}" alt="${country.name}" width="80">
+        <h2>${country.name.official}</h2>
+        </li>`;
     })
     .join('');
-  countryList.insertAdjacentHTML('beforeend', list);
 }
 function createMurkupCard(arr) {
-  const list = arr
-    .map(({ name, capital, population, flags, languages }) => {
-      return ` <div class = "title">
-  <li class = 'country-item'>
-  <img srs ="${flags.svg}" alt ="${name.official}" width = "40x" height ="40px">
-  <p class = "country-name"> ${name.official}</p>
-  </li>
-    </div>`;
+  return arr
+    .map(country => {
+      return `<div class="country-wrapper">
+        <img src="${country.flags.svg}" alt="${country.name}" width="160">
+      <h2 class='country__title'>${country.name.official}</h2>
+      <p class='country__descr'>Capital:<span> ${country.capital}</span></p>
+      <p class='country__descr'>Polulation:<span> ${
+        country.population
+      }</span></p>
+      <p class='country__descr'>Languages:<span> ${Object.values(
+        country.languages
+      )}</span></p></div>`;
     })
     .join('');
 }
+
+const notifyFailureMessage = () => {
+  return Notify.failure('Oops, there is no country with that name');
+};
+
+const notifyInfoMessage = () => {
+  return Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+};
